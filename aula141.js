49ddf293648eb3f488d5858eb2ce8b77408216a6
lincoln1000gomes <59400290+lincoln1000gomes@users.noreply.github.com>
2025-07-13 10:35:01 -0300

    Add files via upload

--- aula141.js
+++ aula141.js
@@ -1,3 +1,21 @@
 import {Login} from "./login.js";
+import { Cxmsg } from "./cxmsg.js";
+const config={
+    cor:"00609C",
+    img:"df.png"
+}
+const callback_ok=()=>{
+    
+}
 
-Login.login("123","321");
+const callback_not=()=>{
+    const config={
+        textos:null ,
+        comando_sn:()=>{console.log("deu certo")},
+        cor:"#800" ,
+        tipo: "ok"
+    }
+    Cxmsg.mostrar(config,"Erro","Login Não Efetuado! Usuário ou Senha incorretos");
+}
+
+Login.login(callback_ok,callback_not,config);
